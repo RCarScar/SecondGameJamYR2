2467368c2647e1de095e4bebcfa586a87268c061
Blake Rubadue <brubadue@outlook.com>
2023-01-18 15:49:00 -0800

    input and game manager - 1

--- Game Jam YR2/Assets/Scripts/GameManager.cs
+++ Game Jam YR2/Assets/Scripts/GameManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+    public static PlayerInput pInput;
+    public static InputActions actions;
+
+    private void Reset()
     {
-        
+        Instance = this; //setup static GM instance
+    }
+
+    void Awake()
+    {
+        // - setup input - //
+        pInput = GetComponent<PlayerInput>();
+        actions = new InputActions();
+        actions.Enable(); 
     }
 
     // Update is called once per frame
