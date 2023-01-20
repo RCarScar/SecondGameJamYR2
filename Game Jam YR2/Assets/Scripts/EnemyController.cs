1baad50950dadda02373067866c0a4dbf9255c1f
RCarScar <90071713+RCarScar@users.noreply.github.com>
2023-01-19 16:30:50 -0800

    Enemy Working

--- Game Jam YR2/Assets/Scripts/EnemyController.cs
+++ Game Jam YR2/Assets/Scripts/EnemyController.cs
@@ -1,26 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 [System.Serializable]
 public class Debugging
 {
     public float velocityX, velocityY, playerDirection;
-    public bool left, right;
+    public bool left, right, peakGravity;
 }
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private PlayerController pc;
+    #region Variables
     [SerializeField] private GameObject player;
-    [SerializeField] private float speed = 1, maxSpeed = 1, acceleration = 1, jumpPower, airAcceleration = 0.2f; 
+    [SerializeField] private float speed = 1, maxSpeed = 1, acceleration = 1, jumpPower, airAcceleration = 0.2f, peakGravity; 
     [SerializeField] private int playerDirection = 1;
 
-    public Debugging db = new Debugging();
+    private float timeKeeper, pastVelocity;
+    private bool jumpPeaked = false;
     private Rigidbody2D rb;
     private float groundAcceleration = 1;
 
@@ -29,10 +25,13 @@ public class EnemyController : MonoBehaviour
     public Vector2 GCPosition;
     public LayerMask GCMask;
 
+    [Header("Debugging Strictly")]
+    public Debugging db = new Debugging();
+    #endregion
+
     void Start()
     {
         groundAcceleration = acceleration;
-        pc = player.GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -46,44 +45,69 @@ public class EnemyController : MonoBehaviour
         db.right = (Physics2D.OverlapCircle((Vector2)transform.position + Vector2.right / 2, GCRadius, GCMask)) ? true : false;
         //Wall Left
         db.left = (Physics2D.OverlapCircle((Vector2)transform.position + Vector2.left / 2, GCRadius, GCMask)) ? true : false;
-
+        db.peakGravity = jumpPeaked;
         #endregion
+        
+        //If at the peak of jump, make gravity faster.
+        GravityChange();
 
+        //If in the air, set the acceleration to the air acceleration.
         acceleration = Grounded ? groundAcceleration : airAcceleration;
 
-        playerDirection = Mathf.RoundToInt(Mathf.Sign(transform.position.x - player.transform.position.x));
+        //The player direction is -1 if left, 1 if right.
+        playerDirection = Mathf.RoundToInt(Mathf.Sign(player.transform.position.x - transform.position.x));
 
+        //Move towards Player
         EnemyMove(playerDirection);
 
+        //Jump if stuck on a block.
         Navigate();
     }
+
+    private void GravityChange()
+    {
+        if(Grounded == true)
+        {
+            rb.gravityScale = 9.8f;
+            jumpPeaked = false;
+            return;
+        }
+
+        if (pastVelocity < rb.velocity.x)
+        {
+            jumpPeaked = true;
+            rb.gravityScale = peakGravity;
+        }
+        else
+        {
+            timeKeeper = Time.time;
+            pastVelocity = rb.velocity.y;
+        }
+    }
+
     private void Navigate()
     {
-        //Wall Left
+        //Left & Right Wall Collision Detection and Velocity Set
         rb.velocity = Physics2D.OverlapCircle((Vector2)transform.position + Vector2.right / 2, GCRadius, GCMask)
-            ? rb.velocity + new Vector2(0, jumpPower)
+            || Physics2D.OverlapCircle((Vector2)transform.position + Vector2.left / 2, GCRadius, GCMask)
+            ? rb.velocity + new Vector2(playerDirection, jumpPower)
             : rb.velocity;
-        //Wall Left
-        rb.velocity = Physics2D.OverlapCircle((Vector2)transform.position + Vector2.left/2, GCRadius, GCMask)
-            ? rb.velocity + new Vector2(0, jumpPower)
-            : rb.velocity;
+    }
+
+    private void EnemyMove(int playerDirection = 1)
+    {
+        //From the current velocity, increase by Acceleration every Fixed Frame until you hit the Max Speed
+        //If on the right side, go to maxSpeed. If on the left side, go to -maxSpeed.
+        rb.velocity = new Vector2(Mathf.MoveTowards(rb.velocity.x, Mathf.Sign(playerDirection) * maxSpeed, Time.fixedDeltaTime * acceleration), rb.velocity.y);
     }
 
     private bool Grounded => Physics2D.OverlapCircle((Vector2)transform.position + Vector2.down, GCRadius, GCMask);
 
-    private void EnemyMove(int playerDirection = 1)
-    {
-        //When on the left, do max speed positive.
-        rb.velocity = playerDirection < 0
-            ? new Vector2(Mathf.MoveTowards(rb.velocity.x, maxSpeed, Time.fixedDeltaTime * acceleration), rb.velocity.y)
-            : new Vector2(Mathf.MoveTowards(rb.velocity.x, -maxSpeed, Time.fixedDeltaTime * acceleration), rb.velocity.y);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere((Vector2)transform.position + Vector2.left/2, GCRadius);
-        Gizmos.DrawSphere((Vector2)transform.position + Vector2.down, GCRadius);
-        Gizmos.DrawSphere((Vector2)transform.position + Vector2.right/2, GCRadius);
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawSphere((Vector2)transform.position + Vector2.left/2, GCRadius);
+    //    Gizmos.DrawSphere((Vector2)transform.position + Vector2.down, GCRadius);
+    //    Gizmos.DrawSphere((Vector2)transform.position + Vector2.right/2, GCRadius);
+    //}
 }
